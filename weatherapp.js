5b545c940acbaa26f2dd84fafe275fd7cb5ff3c7
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-09-13 17:39:04 -0500

    starting base

--- weatherapp.js
+++ weatherapp.js
@@ -1,1 +1,38 @@
-console.log("hey")
+
+// Get the weather 
+// Get the Forecast
+// Save the history
+
+
+
+// first - click events 
+// search click evnt 
+// history click event (on li elements)
+
+
+// second - get local storage
+// key should be "history" and value is array of strings of cities
+// stringify to save 
+// parse history use || []
+
+// if something is in history, search the last element searchWeather(history[history.length-1])
+// make history buttons (list items class llist-group-item list-group-iteem-action)
+
+// Search Weather
+// checks if it exists in history - if not push to llocal storage 
+// call api 
+// got results
+// building current weather 
+// creating cards 
+// call get forecast
+// call get uv index
+
+
+
+
+// make 3 divs 
+// once your data is there work on styling
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
